0782047c6b5206cb296d3f917495b60417ad0c58
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-11-10 18:17:55 +0100

    Update filters

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,10 +1,26 @@
 module.exports = {
     onlyUglifyFailures: function(text) {
-        return text.match(/https:\/\/[a-zA-Z0-9\/\-.]+(?=\) \(Caused by UglifyJS\))/g)
+        return text.match(/https:\/\/[a-zA-Z0-9\/\-._]+(?=\) \(Caused by UglifyJS\))/g)
             .map(function(r, i, a) {
                 return r.substr("https://github.com/tc39/test262/blob/master/test/".length);
             })
             .reduce(function(a, b) {
+                return a + "\n" + b;
+            });
+    },
+    nodeAndUglifyFailures: function(text) {
+        return text.match(/https:\/\/[a-zA-Z0-9\/\-._]+(?=\) \(Caused by Node and by UglifyJS\))/g)
+            .map(function(r, i, a) {
+                return r.substr("https://github.com/tc39/test262/blob/master/test/".length);
+            }).reduce(function(a, b) {
+                return a + "\n" + b;
+            });
+    },
+    unknownFailures: function(text) {
+        return text.match(/https:\/\/[a-zA-Z0-9\/\-._]+(?=\) \-)/g)
+            .map(function(r, i, a) {
+                return r.substr("https://github.com/tc39/test262/blob/master/test/".length);
+            }).reduce(function(a, b) {
                 return a + "\n" + b;
             });
     }
